1c54eb615dbe309c6d8f7361f6030728345d258f
Yannohkym <iankym1997@gmail.com>
2025-04-08 20:32:54 +0300

    update-route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/books', [BookController::class, 'index'])->name('books.index');
+});
+Route::middleware('role:admin')->group(function () {
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
 });
 
 require __DIR__.'/auth.php';
